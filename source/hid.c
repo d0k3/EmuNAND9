01bc68c9c7015ceb050f1a77406fa4de0cc47909
d0k3 <tore.anon@gmail.com>
2016-06-06 22:44:22 +0200

    Improved HID routines

--- source/hid.c
+++ source/hid.c
@@ -5,7 +5,12 @@ u32 InputWait() {
     u32 pad_state_old = HID_STATE;
     while (true) {
         u32 pad_state = HID_STATE;
-        if (pad_state ^ pad_state_old)
+        if (pad_state == pad_state_old)
+            continue;
+        // make sure the key is pressed
+        u32 t_pressed = 0;
+        for(; (t_pressed < 0x13000) && (pad_state == HID_STATE); t_pressed++);
+        if (t_pressed >= 0x13000)
             return ~pad_state;
     }
 }
